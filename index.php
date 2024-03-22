fb482bff4a5c53cfd39e2a1f8ae652577c503530
Diogo Muniz <diogomuniz.web@gmail.com>
2024-03-22 14:14:49 -0300

    Ajuste index - get dados URL

--- index.php
+++ index.php
@@ -4,14 +4,4 @@ header('Content-Type: application/json; charset=utf-8');
 echo "<h2>Olá, aplicação de pé </h2>";
 
 
-if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
-    $url = "https://";
-else
-    $url = "http://";
-// Append the host(domain name, ip) to the URL.   
-$url .= $_SERVER['HTTP_HOST'];
-
-// Append the requested resource location to the URL   
-$url .= $_SERVER['REQUEST_URI'];
-
-echo $url;
+var_dump($_SERVER['HTTPS']);
