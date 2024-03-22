83eaaff2bc4f99285a170d383cdf60809ea70eb6
Diogo Muniz <diogomuniz.web@gmail.com>
2024-03-22 16:45:42 -0300

    Adicionada descrição do serviço no index de teste

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 header('Content-Type: application/json; charset=utf-8');
-echo "<h2>Olá, aplicação de pé </h2>";
+echo "<h2>Olá, aplicação AUTENTICACAO está de pé </h2>";
 
 
 var_dump($_SERVER['HTTPS']);
